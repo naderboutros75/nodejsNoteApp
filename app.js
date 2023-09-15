a019e9b2185b11d7a55225c417de228aa19ec25e
Nader Boutros <132116377+naderboutros75@users.noreply.github.com>
2023-09-15 00:38:11 -0400

    2nd commit 09-15-23

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+app.use(expressEjsLayouts);
+app.set("layout", "./lauout/main");
+app.set("viewengine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`App listing on port ${port}`);
+});
